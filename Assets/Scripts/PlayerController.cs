e6763441d6acf895efc8302f881fc4c663922493
Jan Schenk <jschenk421@yahoo.com>
2018-10-26 11:53:09 +0200

    Rollerball
    Added al relevant rollerball code.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    public Text countText;
+    public Text winText;
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        count = 0;
+        rb = GetComponent<Rigidbody>();
+        countText.text = "Count: " + count.ToString();
+        winText.text = "";
+    }
+
+    // Use this for physics
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement*speed);
+     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            countText.text = "Count: " + count.ToString();
+            if (count == 5)
+            {
+                winText.text = "Gewonnen!";
+                
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
+
+  
